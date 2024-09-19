b4aca93ce7b1dd2368f8fc2a6334a97527262783
Savvina <savvina_daniil@hotmail.com>
2024-09-19 15:37:24 +0200

    Issue with fetching

--- Scholarly_test.py
+++ Scholarly_test.py
@@ -1,18 +1,29 @@
 import os
 os.system("clear")
-from scholarly import scholarly
+print('hi')
+from scholarly import scholarly, ProxyGenerator
+print('hi')
+import pickle as pkl
+
+
+pg = ProxyGenerator() 
+success = pg.FreeProxies()
+scholarly.use_proxy(pg)
 
 search_query = scholarly.search_pubs('allintitle: (book OR books OR author OR authors OR library OR libraries) (recommend OR recommended OR recommending OR recommends OR recommendation OR recommendations OR recommender OR recommenders OR "artificial intelligence" OR AI OR "machine learning") (ethics OR ethical OR responsible OR bias OR fair OR diverse OR diversity OR diversification OR fairness OR gender OR sex OR nationality OR ethnicity OR race OR religion)')
+print('hi')
+
 print(search_query)
 # scholarly.pprint(next(search_query))
 
 nr_results = search_query.total_results
-pub_parser = search_query.pub_parser
+# pub_parser = search_query.pub_parser
 
 print(nr_results)
 # print(pub_parser.bibtex)
 # print(help(pub_parser))
 i = 0
+all_results = []
 while i<nr_results:
     current_result = next(search_query)
     
@@ -26,3 +37,7 @@ while i<nr_results:
     
     i+=1
     print(i)
+    all_results.append(current_result)
+
+with open('google_results.pkl', 'wb') as handle:
+    pkl.dump(all_results, handle, protocol=pickle.HIGHEST_PROTOCOL)
