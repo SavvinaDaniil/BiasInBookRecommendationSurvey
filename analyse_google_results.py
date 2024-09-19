19fec9c2ab5d888cca414ee74587c4ba6d805a7e
Savvina Daniil <savvina@cwi.nl>
2024-09-19 15:58:30 +0200

    Create results table

---
+++ analyse_google_results.py
@@ -0,0 +1,25 @@
+import os
+os.system("clear")
+import pickle as pkl
+import pandas as pd
+
+
+with open('google_results.pkl', 'rb') as handle:
+    all_results = pkl.load(handle)
+
+res_df = pd.DataFrame(columns = ['Title','Authors','Venue', 'Year', 'Abstract','Link'])
+for i in range(len(all_results)):
+    result = all_results[i]
+    info = result['bib']
+    res_df.at[i, 'Title'] = info['title']
+    res_df.at[i, 'Authors'] = info['author']
+    res_df.at[i, 'Venue'] = info['venue']
+    res_df.at[i, 'Year'] = info['pub_year']
+    res_df.at[i, 'Abstract'] = info['abstract']
+    try:
+        res_df.at[i, 'Link'] = result['pub_url']
+    except:
+        res_df.at[i, 'Link'] = 'missing'
+        
+print(res_df)
+res_df.to_csv('google_results_table.csv',index=False)
--- download_from_google.py
+++ download_from_google.py
@@ -6,9 +6,9 @@ print('hi')
 import pickle as pkl
 
 
-pg = ProxyGenerator() 
-success = pg.FreeProxies()
-scholarly.use_proxy(pg)
+# pg = ProxyGenerator() 
+# success = pg.FreeProxies()
+# scholarly.use_proxy(pg)
 
 search_query = scholarly.search_pubs('allintitle: (book OR books OR author OR authors OR library OR libraries) (recommend OR recommended OR recommending OR recommends OR recommendation OR recommendations OR recommender OR recommenders OR "artificial intelligence" OR AI OR "machine learning") (ethics OR ethical OR responsible OR bias OR fair OR diverse OR diversity OR diversification OR fairness OR gender OR sex OR nationality OR ethnicity OR race OR religion)')
 print('hi')
@@ -40,4 +40,4 @@ while i<nr_results:
     all_results.append(current_result)
 
 with open('google_results.pkl', 'wb') as handle:
-    pkl.dump(all_results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pkl.dump(all_results, handle, protocol=pkl.HIGHEST_PROTOCOL)
