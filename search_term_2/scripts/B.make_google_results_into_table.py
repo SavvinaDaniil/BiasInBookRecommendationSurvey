4f299797bd50d80fa96e3a71d1c040797766a7ac
Savvina <savvina_daniil@hotmail.com>
2024-10-03 12:03:16 +0200

    Google results

--- search_term_2/scripts/B.make_google_results_into_table.py
+++ search_term_2/scripts/B.make_google_results_into_table.py
@@ -7,40 +7,21 @@ import pandas as pd
 with open('search_term_2/datasets/google_results.pkl', 'rb') as handle:
     all_results = pkl.load(handle)
     
-print(len(all_results))
+res_df = pd.DataFrame(columns = ['Title','Authors','Venue', 'Year', 'Abstract','Link'])
 
-# print(all_results)
-results = [x for xs in all_results for x in xs]
-print(len(results))
-fr = results[0]
-print(fr.keys())
-print(fr['title'], fr['link'], [x['name'] for x in fr['publication_info']['authors']], fr['snippet'], fr['resources'])
-print(fr['publication_info']['summary'])
 
-for key in fr.keys():
-    print(key, ":", fr[key])
-    print('\n')
-# print(all_results)
-
-results = [x for x in results if x!='']
-res_df = pd.DataFrame(columns = ['Title','Authors','Abstract','Link'])
-for i in range(len(results)):
-    result = results[i]
+for i in range(len(all_results)):
+    result = all_results[i]
+    info = result['bib']
+    res_df.at[i, 'Title'] = info['title']
+    res_df.at[i, 'Authors'] = info['author']
+    res_df.at[i, 'Venue'] = info['venue']
+    res_df.at[i, 'Year'] = info['pub_year']
+    res_df.at[i, 'Abstract'] = info['abstract']
     try:
-        res_df.at[i, 'Title'] = result['title']
-    except:
-        res_df.at[i, 'Title'] = 'missing'
-    try:
-        res_df.at[i, 'Authors'] = [x['name'] for x in result['publication_info']['authors']]
-    except:
-        res_df.at[i, 'Authors'] = 'missing'
-    try:
-        res_df.at[i, 'Abstract'] = result['snippet']
-    except:
-        res_df.at[i, 'Abstract'] = 'missing'
-    try:
-        res_df.at[i, 'Link'] = result['link']
+        res_df.at[i, 'Link'] = result['pub_url']
     except:
         res_df.at[i, 'Link'] = 'missing'
 # print(res_df)
 res_df.to_csv('search_term_2/datasets/google_results_table.csv',index=False)
+print(len(res_df))
